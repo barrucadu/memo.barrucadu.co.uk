5e887b0568a5f302df7536369a64ff605f88c338
Michael Walker <mike@barrucadu.co.uk>
2021-02-05 18:55:43 +0000

    Add a `draft` flag

--- local-build.sh
+++ local-build.sh
@@ -4,4 +4,4 @@ docker run -it --rm -v $(pwd):/src -v $HOME/http/_site:/build -w /src registry.b
   virtualenv venv
   source venv/bin/activate
   pip install -r requirements.txt
-  ./build --root=https://misc.barrucadu.co.uk/_site/ --out=/build"
+  ./build --drafts --root=https://misc.barrucadu.co.uk/_site/ --out=/build"
