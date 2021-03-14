09c38defcbc533d01cda27a47f92f5e9d005cf6e
Michael Walker <mike@barrucadu.co.uk>
2021-03-14 13:32:03 +0000

    Autodetect --root in local-build.sh

--- local-build.sh
+++ local-build.sh
@@ -1,7 +1,13 @@
 #!/bin/sh
 
+ROOT="https://misc.barrucadu.co.uk/_site/"
+
+if [[ "$(hostname)" == "azathoth" ]]; then
+  ROOT="file:///home/barrucadu/http/_site/"
+fi
+
 docker run -it --rm -v $(pwd):/src -v $HOME/http/_site:/build -w /src registry.barrucadu.dev/barrucadu.co.uk-builder sh -c "
   virtualenv venv
   source venv/bin/activate
   pip install -r requirements.txt
-  ./build --drafts --root=https://misc.barrucadu.co.uk/_site/ --out=/build"
+  ./build --drafts --root=$ROOT --out=/build"
