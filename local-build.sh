07c2bb8c54efd05e6fa805c37dc72c77618a68e2
Michael Walker <mike@barrucadu.co.uk>
2021-04-18 20:17:10 +0100

    Fix image reference in local-build.sh

--- local-build.sh
+++ local-build.sh
@@ -6,7 +6,7 @@ if [[ "$(hostname)" == "azathoth" ]]; then
   ROOT="file:///home/barrucadu/http/_site/"
 fi
 
-docker run -it --rm -v $(pwd):/src -v $HOME/http/_site:/build -w /src registry.barrucadu.dev/barrucadu.co.uk-builder sh -c "
+docker run -it --rm -v $(pwd):/src -v $HOME/http/_site:/build -w /src registry.barrucadu.dev/memo-builder sh -c "
   virtualenv venv
   source venv/bin/activate
   pip install -r requirements.txt
