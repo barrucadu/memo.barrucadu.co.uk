84b8ce57c9bd891037512a224958b8fcf86076ba
Michael Walker <mike@barrucadu.co.uk>
2023-08-23 14:58:58 +0100

    MAke local-build.sh work with both docker and podman

--- local-build.sh
+++ local-build.sh
@@ -6,7 +6,13 @@ if [[ "$(hostname)" == "azathoth" ]]; then
   ROOT="file:///home/barrucadu/http/_site/"
 fi
 
-docker run -it --rm -v $(pwd):/src -v $HOME/http/_site:/build -w /src registry.barrucadu.dev/memo-builder sh -c "
+if command -v docker &>/dev/null; then
+  docker="docker"
+else
+  docker="podman"
+fi
+
+$docker run -it --rm -v $(pwd):/src -v $HOME/http/_site:/build -w /src registry.barrucadu.dev/memo-builder sh -c "
   virtualenv venv
   source venv/bin/activate
   pip install -r requirements.txt
